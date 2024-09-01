7ac15493f21d0d4b595ea6e577dd00945dc2984d
Martin Chapman <contact@martinchapman.co.uk>
2024-09-01 12:04:54 +0100

    Update made by Phenoflow

---
+++ python/autoimmune-hemolytic-anemia-coldtype---secondary.py
@@ -0,0 +1,25 @@
+# phekb, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"4131128","system":"ICD10CM"},{"code":"4131917","system":"ICD10CM"},{"code":"4174376","system":"ICD10CM"},{"code":"4271197","system":"ICD10CM"},{"code":"4273847","system":"ICD10CM"},{"code":"4273847","system":"ICD10CM"},{"code":"4174376","system":"ICD10CM"},{"code":"4271197","system":"ICD10CM"},{"code":"4131128","system":"ICD10CM"},{"code":"4131917","system":"ICD10CM"},{"code":"4131128","system":"ICD10CM"},{"code":"4131917","system":"ICD10CM"},{"code":"4174376","system":"ICD10CM"},{"code":"4271197","system":"ICD10CM"},{"code":"4273847","system":"ICD10CM"},{"code":"4273847","system":"ICD10CM"},{"code":"4174376","system":"ICD10CM"},{"code":"4271197","system":"ICD10CM"},{"code":"4131128","system":"ICD10CM"},{"code":"4131917","system":"ICD10CM"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('autoimmune-hemolytic-anemia-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["autoimmune-hemolytic-anemia-coldtype---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["autoimmune-hemolytic-anemia-coldtype---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["autoimmune-hemolytic-anemia-coldtype---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
